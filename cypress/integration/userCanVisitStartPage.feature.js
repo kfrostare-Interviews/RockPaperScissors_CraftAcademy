66afba82703972d1c76dc32a0eb369b782a6d2c2
karolina <karolina.frostare@gmail.com>
2020-02-24 18:11:06 +0100

    creates game- and score board class

--- cypress/integration/userCanVisitStartPage.feature.js
+++ cypress/integration/userCanVisitStartPage.feature.js
@@ -4,5 +4,6 @@ describe('User can visit the start page', () => {
 	it('user can enter the start page', () => {
 		cy.visit('http://localhost:3001')
 		cy.contains('Rock Paper Scissors')
+		
 	})
 })
