1554884988a12ff95e0c0ab2259f7ff6796878ab
karolina <karolina.frostare@gmail.com>
2020-02-24 19:11:49 +0100

    splits one test in three parts

---
+++ cypress/integration/userCanInteractOnGamePage.feature.js
@@ -0,0 +1,20 @@
+// Player can interact with game page 
+describe('User can navigate the Game page', () => {
+
+    it('player can see score board changed to $500 for both players', () => {})
+    it('player can see dollar icons in the score board next to score', () => {})
+    it('player can see title on game page "Make your choice"', () => {})
+    it('player can see background image on game page', () => {})
+    it('player can see hand icons on each side of the title', () => {})
+    it('player can see the icon they chose for player on left side', () => {})
+    it('player can see one of the other icons as player on left side', () => {})
+    it('player can see a bug icon for player on left side', () => {})
+    it('player can see three buttons giving options of rock, paper or scissors', () => {})
+    it('player can click on one of the options/buttons ', () => {})
+    it('player can see hand icons moving up and down', () => {})
+    it('player can see title change to announce winner', () => {})
+    it('player can see score board change if they win/lose', () => {})
+    it('player can keep playing until they win computers- or lose their own money', () => {})
+    it('player can ', () => {})
+
+)}
---
+++ cypress/integration/userCanInteractOnStartPage.feature.js
@@ -0,0 +1,18 @@
+// Player can interact with start page
+describe('User can navigate the Start Page', () => {
+	
+	it('user can enter the start page', () => {
+		cy.visit('http://localhost:3001')
+	});
+	
+	it('player can see title on start page', () => {})
+	it('player can see image on start page', () => {})
+	it('player can see dollar icons in the score board', () => {})
+	it('player can see score board starting on $0 for both players', () => {})
+	it('player can see 5 icons/avatars on start page', () => {})
+	it('player can see "pick a player"- message on start page', () => {})
+	it('player can click on one player', () => {})
+	it('when user clicks on a player they get sent to game page ', () => {})
+	it('player can ', () => {})
+
+})
