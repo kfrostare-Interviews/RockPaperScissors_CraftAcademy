3ea0920fe2ba71e258e7ea137eb0c5865e15dbbe
karolina <karolina.frostare@gmail.com>
2020-02-24 08:53:26 +0100

    currently unable to access localhost3001, get Cannot POST

---
+++ cypress/integration/userCanVisitStartPage.feature.js
@@ -0,0 +1,7 @@
+// User can enter the start page of our game
+
+describe('User can visit the start page', () => {
+	it('user can enter the start page', () => {
+		cy.visit('http://localhost:3001')
+	})
+})
