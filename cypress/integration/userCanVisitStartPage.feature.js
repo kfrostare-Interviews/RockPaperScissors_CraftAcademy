eb041b1c92cdaa20e6255e29dc9b6c04300de9ec
karolina <karolina.frostare@gmail.com>
2020-02-24 18:16:04 +0100

    updates tests for user being able to view score board

--- cypress/integration/userCanVisitStartPage.feature.js
+++ cypress/integration/userCanVisitStartPage.feature.js
@@ -4,6 +4,12 @@ describe('User can visit the start page', () => {
 	it('user can enter the start page', () => {
 		cy.visit('http://localhost:3001')
 		cy.contains('Rock Paper Scissors')
-		
+	});
+
+	it('user can see score board', () => {
+		cy.contains('Player')
+		cy.contains('Computer')
+		cy.contains('0')
 	})
+
 })
