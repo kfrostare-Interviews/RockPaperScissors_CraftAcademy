8c5277dd73762569a11ca9f258354592a47f7dc2
karolina <karolina.frostare@gmail.com>
2020-02-28 07:26:23 +0100

    adds div class 'startOptions' to querySelector for fadeout onClick

--- src/js/gameLogic.js
+++ src/js/gameLogic.js
@@ -7,13 +7,14 @@ const game = () => {
   // this is the "start game"- function, it just fades the pages, 
   // I make it its own constant because I'll only use it once.
   const startGame = () => {
-    const playBtn = document.querySelector('.startPage button');
+    const playBtn = document.querySelector('.startOptions');
     const startScreen = document.querySelector('.startPage');
     const gameScreen = document.querySelector('.gamePage');
     const gameOptions = document.querySelector('.gameOptions')
     
     // every time you click on the startPage button the following events will run
     playBtn.addEventListener('click', () => {
+      playBtn.classList.add('fadeOut')
       startScreen.classList.add('fadeOut'); // when somebody clicks a button on the start page we want to fade the start page out
       gameScreen.classList.add('fadeIn');
       gameOptions.classList.add('fadeIn');
