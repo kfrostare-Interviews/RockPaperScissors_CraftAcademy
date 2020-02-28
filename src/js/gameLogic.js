65aa69372149d9c5d8af4881de6c3cc45f9542e3
karolina <karolina.frostare@gmail.com>
2020-02-28 06:05:34 +0100

    updates game page with fadeout so I can focus on start page

---
+++ src/js/gameLogic.js
@@ -0,0 +1,9 @@
+// this function is the whole game
+const game = () => {
+  // this is the score in the score board
+  let pScore = 0; 
+  let cScore = 0;
+
+  // this is the "start game"- function, it just fades the start page
+
+}
