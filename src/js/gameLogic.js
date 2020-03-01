6dbe8736fe05b187b1ba17b0969ea1db610b1c7a
karolina <karolina.frostare@gmail.com>
2020-03-01 18:29:49 +0100

    updates options- function, goes through game logic to find bug

--- src/js/gameLogic.js
+++ src/js/gameLogic.js
@@ -7,17 +7,17 @@ const game = () => {
   // this is the "start game"- function, it just fades the pages, 
   // I make it its own constant because I'll only use it once.
   const startGame = () => {
-    const playBtn = document.querySelector('.startOptions');
-    const startScreen = document.querySelector('.startPage');
-    const gameScreen = document.querySelector('.gamePage');
-    const gameOptions = document.querySelector('.gameOptions')
+    const playBtn = document.querySelector(".startOptions");
+    const startScreen = document.querySelector(".startPage");
+    const gameScreen = document.querySelector(".gamePage");
+    const gameOptions = document.querySelector(".gameOptions");
     
     // every time you click on the startPage button the following events will run
-    playBtn.addEventListener('click', () => {
-      playBtn.classList.add('fadeOut')
-      startScreen.classList.add('fadeOut');
-      gameScreen.classList.add('fadeIn');
-      gameOptions.classList.add('fadeIn');
+    playBtn.addEventListener("click", () => {
+      playBtn.classList.add("fadeOut");
+      startScreen.classList.add("fadeOut");
+      gameScreen.classList.add("fadeIn");
+      gameOptions.classList.add("fadeIn");
     });
   };
 
@@ -25,20 +25,20 @@ const game = () => {
   const playGame = () => {
 
     // we need to give the player their options for buttons
-    const gameOptions = document.querySelectorAll('.gameOptions button');
+    const gameOptions = document.querySelectorAll(".gameOptions button");
 
     // now we're getting the player their images
-    const playerHand = document.querySelector('.playerHand');
-    const computerHand = document.querySelector('.computerHand');
+    const playerHand = document.querySelector(".playerHand");
+    const computerHand = document.querySelector(".computerHand");
     
     // now we're getting the computer's options and for this we are
     // actually generating a random number between 1-3 that we will then
     // associate to an image (rock, paper or scissors)
-    const computerOptions = ['rock', 'paper', 'scissors']; // <--- Array with three string options
+    const computerOptions = ["rock", "paper", "scissors"]; // <--- Array with three string options
 
     // we only want things to run after we click on one of the buttons, so let's bring them in
-    computerOptions.forEach((gameOptions) => {
-      option.addEventListener('click', function(){
+    gameOptions.forEach(option => {
+      option.addEventListener("click", function() {
         // this variable will be set equal to a random- function
         // the random- function by itself only generate a random number between 0-1
         // therefor we add * 3 to get three options to choose from
@@ -60,54 +60,52 @@ const game = () => {
   // now we need a comparison function so we know who is winning
   const compareHands = (playerChoice, computerChoice) => {
     // update text
-    const winner = document.querySelector('.winner');
+    const winner = document.querySelector(".winner");
     
     // Rules for a tie
     if(playerChoice === computerChoice){
-        winner.textContent = 'It is a tie';
+        winner.textContent = "It is a tie";
         return;
     }
     // Rules for user choosing Rock
-    if(playerChoice === 'rock'){
-      if(computerChoice === 'scissors'){
-        winner.textContent = 'player wins';
+    if(playerChoice === "rock"){
+      if(computerChoice === "scissors"){
+        winner.textContent = "player wins";
         return;
       }else{
-        winner.textContent = 'Computer wins';
+        winner.textContent = "Computer wins";
         return;
       }
     }
     // Rules for user choosing Paper
-    if(playerChoice === 'paper'){
-      if(computerChoice === 'scissors'){
-        winner.textContent = 'computer wins';
+    if(playerChoice === "paper"){
+      if(computerChoice === "scissors"){
+        winner.textContent = "computer wins";
         return;
       }else{
-        winner.textContent = 'Player wins';
+        winner.textContent = "Player wins";
         return;
       }
     }
 
     // Rules for user choosing Scissors
-    if(playerChoice === 'scissors'){
-      if(computerChoice === 'rock'){
-        winner.textContent = 'computer wins';
+    if(playerChoice === "scissors"){
+      if(computerChoice === "rock"){
+        winner.textContent = "computer wins";
         return;
       }else{
-        winner.textContent = 'Player wins';
+        winner.textContent = "Player wins";
         return;
       }
     }
-
-
   }
 
 
 
   // this calls/executes the inner functions 
   //(needs to be commented out while running Cypress)
+  startGame();
   playGame();
-  playMatch();
 
 };
 
