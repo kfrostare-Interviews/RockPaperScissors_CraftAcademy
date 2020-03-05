b0fc42b0da8d4261e6b46606972ed4ac50cc877a
karolina <karolina.frostare@gmail.com>
2020-03-05 07:40:01 +0100

    updates tests for sad path

--- cypress/integration/userCanPlayGameSadPath.feature.js
+++ cypress/integration/userCanPlayGameSadPath.feature.js
@@ -1,12 +1,58 @@
-// Player chooses the sad path
 describe('User chooses to go with the Sad Path', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
+  })
 
-    it('player can see start- page, not any other page', () => {})
+  // Making sure start page displays the start page
+  describe('user can see the start page and it', () {
+
+    it('does not display the game title', () => {
+      cy.get('.winner').should('not.be.visible')
+    });
+
+    it('does not display the player hand', () => {
+      cy.get('.playerHand').should('not.be.visible')
+    });
+
+    it('does not display the computer hand', () => {
+      cy.get('.computerHand').should('not.be.visible')
+    });
+
+    it('does not give option to click on rock- button', () => {
+      cy.get('.rock').should('not.be.visible')
+    });
+
+    it('does not give option to click on paper- button', () => {
+      cy.get('.paper').should('not.be.visible')
+    });
+
+    it('does not give option to click on scissors- button', () => {
+      cy.get('.scissors').should('not.be.visible')
+    });
+  });
+
+  // Making sure game page displays the game page
+  describe('user can see the game page and it', () {
+
+    it('does not display the start title', () => {
+      cy.get('.start').should('not.be.visible')
+    });
+
+    it('does not display the start paragraph', () => {
+      cy.get('.startPage p').should('not.be.visible')
+    });
+
+    it('does not display the start button', () => {
+      cy.get('.button').should('not.be.visible')
+    });
+  });
+
+})
+
     it('player can see game- page, not any other page ', () => {})
     it('player tries to click on score board', () => {})
     it('player tries to click on two options for player at the same time', () => {})
     it('player tries to click on two options for rock, paper, scissors at the same time', () => {})
     it('player tries to click on -back button mid game', () => {})
     it('player tries to alter score board ', () => {})
-
-})
+    it('player gets the right winner messages', () => {})
